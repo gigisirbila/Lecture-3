429feb48e50084fe13506f0416e66c16fe86b709
gigi sirbilashvili <gigi.sirbilashvili@gmail.com>
2025-12-28 15:37:05 +0400

    update randomDirective to change bg color

--- lc3/src/app/bg-random/bg-random.ts
+++ lc3/src/app/bg-random/bg-random.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RandomDirective } from '../random-directive';
-
 @Component({
   selector: 'app-bg-random',
   imports: [RandomDirective],
--- lc3/src/app/randomDirective.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RandomDirective } from './randomDirective';
-
-describe('Random', () => {
-  it('should create an instance', () => {
-    const directive = new RandomDirective();
-    expect(directive).toBeTruthy();
-  });
-});
